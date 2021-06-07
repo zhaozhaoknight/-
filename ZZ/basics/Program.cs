16e5a1a45d18952d3f04327598c6d8e0a352aa3b
zhao <183754929@qq.com>
2021-06-07 18:21:39 +0800

    2

---
+++ ZZ/basics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+
+    public class A
+    {
+        public A()
+        {
+            Asd();
+        }
+        public virtual void Asd() { }
+    }
+    public class B : A
+    {
+        int x = 1; int y;
+        public B()
+        {
+            y = 2;
+        }
+        public override void Asd()
+        {
+            Console.WriteLine("x={0},y={1}", x, y);
+        }
+    }
+}
